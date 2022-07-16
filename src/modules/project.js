033f98da4dba8653af18d72f491068c771369d22
Selt0 <chewedgum12@gmail.com>
2022-07-15 19:32:12 -0500

    Add static method to project module

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,20 +2,31 @@ class Project {
     constructor(title){
         this.title = title
         this.tasks = []
+        Project.allProjects.push(this)
     }
 
-    getTasks(){
-        return this.tasks
+    static allProjects = []
+
+    static getProject(projectTitle){
+        return Project.allProjects.find(project => project.title === projectTitle)
+    }
+
+    static removeProject(projectTitle){
+        Project.allProjects = Project.allProjects.filter(project => project.title !== projectTitle)
     }
     
     setTasks(tasks){
         this.tasks = tasks
     }
 
+    get length(){
+        return this.tasks.length
+    }
+
     getTask(taskItem){
         const task = this.tasks.find(task => task.name === taskItem.name && task.dateCreated === taskItem.dateCreated)
 
-        return task ? task : false
+        return task
     }
 
     setTask(task){
@@ -26,11 +37,9 @@ class Project {
         this.tasks = this.tasks.filter(task => task.name != taskItem.name && task.dateCreated != taskItem.dateCreated)
     }
 
-    getTitle(){
-        return this.title
-    }
-
     setNewTitle(value){
         this.title = value
     }
 }
+
+export { Project }
